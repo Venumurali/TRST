f3314394d4f6ad37da87a74a00dfcf383fd00e68
venumurali <venumuralitinku@gmail.com>
2019-09-25 15:02:37 +0530

    c

--- M2/src/LoneTeens/LoneTeen.java
+++ M2/src/LoneTeens/LoneTeen.java
@@ -27,9 +27,10 @@ public class LoneTeen {
      */
     public static boolean loneTeen(int a, int b) {
         // Your code goes here....
-        if(((a<=19 || a>=13 ) && (b>19||b<13) ) || ( ( b<=19 || b>=13 ) && ( a>19 || a<13 ) ) ){
+        if(((a<=19 && a>=13 ) && (b>19||b<13) ) || ( ( b<=19 && b>=13 ) && ( a>19 || a<13 ) ) ){
             return true;
         }
-        return false;
+        else{
+        return false;}
     }
 }
